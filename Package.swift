1e2c9b7056ed39b839b664eb5ff89208ed844db5
Ty Hopp <tyhopp@users.noreply.github.com>
2023-04-07 21:54:07 +0800

    Test more platforms in CI (#4)
    * Test more platforms in CI
    
    * Select older Xcode
    
    * Add platform support in readme
    
    * Reduce swift-tools-version to match selected Xcode version in CI
    
    * Remove show destinations step
    
    * Select device names

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
-// swift-tools-version: 5.7.1
+// swift-tools-version: 5.5.0
 
 import PackageDescription
 
 let package = Package(
     name: "Keychain",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
     products: [
         .library(
             name: "Keychain",
